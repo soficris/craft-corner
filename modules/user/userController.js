bce9ee1c103788ec1ad1345c51fd87a35c5e32f0
soficris <sofia.cris.silva@gmail.com>
2026-05-09 23:42:43 -0300

    feat: cadastro de usuário e hash de senha

---
+++ modules/user/userController.js
@@ -0,0 +1,44 @@
+const User = require ('./userModel'); 
+const bcrypt = require('bcryptjs'); 
+
+exports.register = async (req, res) => {
+    const { username, email, password, confirmPassword, fullName } = req.body;
+
+    try{
+        // 1. Validação básica de senhas coincidentes 
+        if(password !== confirmPassword){
+            req.flash('error', 'As senhas não coincidem'); 
+            return res.redirect('/register'); 
+        } 
+
+        //2. Verificar se usuário ou email já existem 
+        const emailExists = await User.findOne({where: {email}}); 
+        const usernameExists = await User.findOne({where: {username}}); 
+
+        if(emailExists || usernameExists){
+            req.flash('error', 'Este email ou usuário já está cadastrado');
+            return res.redirect('/register'); 
+
+        }
+
+        //3. Hash da senha 
+        const salt = await bcrypt.genSalt(10); 
+        const hashedPassword = await bcrypt.hash(password,salt); 
+
+        //4. Salvar no banco
+        await User.create({
+            username, 
+            email, 
+            password: hashedPassword, 
+            fullName
+        }); 
+
+        //5.Redirecionar para login com mensagem de sucesso
+        req.flash('sucess', 'Conta criada com sucesso! Faça seu login'); 
+        res.redirect('/login');
+    } catch(error){
+        console.error(error); 
+        req.flash('error', 'Erro ao criar conta. Ferique os dados e tente novamente.'); 
+        res.redirect('/register'); 
+    }
+}; 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../modules/user/userController'); 
 
 // Rota para a página inicial
 router.get('/', function (req, res, next) {
     res.render('index', { title: 'Craft Corner' });
 });
+
+// Rota para a página de registro
+router.get('/register', (req,res) => {
+    res.render('register', {title: 'Criar Conta'}); 
+}); 
+
+// Rota para processar o formulário de registro
+router.post('/register', userController.register); 
+
+router.get('/login', (req,res) => {
+    res.render('login', { title: 'Entrar'}); 
+}); 
 
 module.exports = router;
